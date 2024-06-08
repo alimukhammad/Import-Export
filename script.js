bd0d1bf4dce7f4b9bd327d5d3ae026143fa20617
sardor <alimukhammad1@gmail.com>
2024-06-07 23:47:41 -0400

    1

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import Papa from 'papaparse';
+
 document.getElementById('fileUploader').addEventListener('change', function() {
     var exportBtn = document.getElementById('exportBtn');
     var importBtn = document.getElementById('importBtn');
@@ -9,4 +11,28 @@ document.getElementById('fileUploader').addEventListener('change', function() {
         exportBtn.classList.remove('hidden');
         importBtn.classList.add('hidden');
     }
+
+    Papa.parse(file, config);
+    console.log('File uploaded', this.files[0]);
+
+    $('input[type=file]').parse({
+        config: {
+            // base config to use for each file
+            // any, delimiter = ""; // auto-detect
+        },
+        before: function(file, inputElem)
+        {
+            // executed before parsing each file begins;
+            // what you return here controls the flow
+        },
+        error: function(err, file, inputElem, reason)
+        {
+            // executed if an error occurs while loading the file,
+            // or if before callback aborted for some reason
+        },
+        complete: function()
+        {
+            // executed after all files are complete
+        }
+    });
 });
